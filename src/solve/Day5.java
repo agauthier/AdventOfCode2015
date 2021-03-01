611b3d9ab9b2b4bb75975714d301a44c5b9f04d3
Alex <alexandregauthier14@gmail.com>
2021-03-01 13:33:02 -0500

    Completed problem 5A.

--- src/solve/Day5.java
+++ src/solve/Day5.java
@@ -1,6 +1,9 @@
 package solve;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import tools.Utils;
 
@@ -13,7 +16,12 @@ class Day5 {
     private static final List<String> fileLines = Utils.readFile("day5.txt");
 
     static String solveA() {
-        return (1 == 0 ? SUCCESS : FAILED) + "";
+        List<String> lines = new ArrayList<>(fileLines);
+        Pattern vowelMatch = Pattern.compile("(.*[aeiou].*){3,}");
+        Pattern dblLetterMatch = Pattern.compile(".*([abcdefghijklmnopqrstuvwxyz])\\1.*");
+        Pattern illegalLetterMatch = Pattern.compile(".*(ab|cd|pq|xy).*");
+        lines.removeIf(line -> !vowelMatch.matcher(line).matches() || !dblLetterMatch.matcher(line).matches() || illegalLetterMatch.matcher(line).matches());
+        return (lines.size() == 236 ? SUCCESS : FAILED) + "Number of nice strings: " + lines.size();
     }
 
     static String solveB() {
